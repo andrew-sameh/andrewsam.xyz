1ceb1008963cf9ecf3899922a88f05f9bd733b68
FLAIRSTECH\andrew.sameh <andrew.sameh@innquest.com>
2024-06-21 09:38:18 +0300

    hotfix error page metadata

--- app/(landing)/auth/error/layout.tsx
+++ app/(landing)/auth/error/layout.tsx
@@ -1,5 +1,8 @@
 import SectionContainer from '@/components/SectionContainer'
 
+import { genPageMetadata } from 'app/seo'
+export const metadata = genPageMetadata({ title: 'Authentication Error' })
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return <div className="">{children}</div>
 }
--- app/(landing)/auth/error/page.tsx
+++ app/(landing)/auth/error/page.tsx
@@ -3,8 +3,7 @@ import { useSearchParams } from 'next/navigation'
 import Image from 'next/image'
 import SiteLogo from '@/components/SiteLogos'
 
-import { genPageMetadata } from 'app/seo'
-export const metadata = genPageMetadata({ title: 'Authentication Error' })
+
 
 export default function AuthErroPage() {
   // error passed in query params as ?error=
